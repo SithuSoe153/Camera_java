37898ee41a4e9d5678bfe4b1b6ec4b08b192c001
Sithu_Soe <sithu032001@gmail.com>
2023-10-09 20:21:02 +0630

    working code

---
+++ app/src/main/java/com/uog/cameraapp/MainActivity.java
@@ -0,0 +1,95 @@
+package com.uog.cameraapp;
+
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.renderscript.ScriptGroup;
+import android.widget.Toast;
+
+import com.uog.cameraapp.databinding.ActivityMainBinding;
+
+import java.io.File;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int CAMERA_PERMISSION_CODE = 1;
+    ActivityMainBinding mainBinding;
+    ActivityResultLauncher<Uri> takePictureLauncher;
+    Uri imageUri;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mainBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(mainBinding.getRoot());
+
+        imageUri = createUri();
+        registerPictureLauncher();
+
+        mainBinding.btnTakePicture.setOnClickListener(view -> {
+            checkCameraPermissionAndOpenCamera();
+
+        });
+    }
+    private Uri createUri(){
+
+        File imageFile = new File(getApplicationContext().getFilesDir(), "camera_photo.jpg");
+        return FileProvider.getUriForFile(
+                getApplicationContext(),
+                "com.uog.cameraapp.fileProvider",
+                imageFile
+        );
+    }
+
+    private void registerPictureLauncher(){
+        takePictureLauncher = registerForActivityResult(
+                new ActivityResultContracts.TakePicture(),
+                new ActivityResultCallback<Boolean>() {
+                    @Override
+                    public void onActivityResult(Boolean result) {
+                        try {
+                            if (result){
+                                mainBinding.ivUser.setImageURI(null);
+                                mainBinding.ivUser.setImageURI(imageUri);
+                            }
+                        }catch (Exception exception){
+                            exception.getStackTrace();
+                        }
+                    }
+                }
+        );
+    }
+
+    private void checkCameraPermissionAndOpenCamera(){
+        if (ActivityCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.CAMERA)!= PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[]{Manifest.permission.CAMERA}, CAMERA_PERMISSION_CODE);
+        }else {
+            takePictureLauncher.launch(imageUri);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == CAMERA_PERMISSION_CODE){
+            if (grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                takePictureLauncher.launch(imageUri);
+            }else{
+                Toast.makeText(this,"Camera Permission Denied", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+}
